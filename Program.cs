9b69028e9c4ef8d6b193361ee6f714a01f33f650
Rivera-CuentaOficial <146663110+Rivera-CuentaOficial@users.noreply.github.com>
2025-09-25 00:46:36 -0300

    feat(Faker): add faker configuration

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Serilog;
 
@@ -7,8 +8,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
-var app = builder.Build();
-
 #region Logging Configuration
 builder.Host.UseSerilog(
     (context, services, configuration) =>
@@ -16,11 +15,47 @@ builder.Host.UseSerilog(
 );
 #endregion
 
+#region Identity Configuration
+Log.Information("Configuring Identity...");
+builder
+    .Services.AddIdentity<User, Role>(options =>
+    {
+        //Password settings
+        options.Password.RequireDigit = true;
+        options.Password.RequiredLength = 8;
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequireUppercase = true;
+        options.Password.RequireLowercase = true;
+
+        //Email settings
+        options.User.RequireUniqueEmail = true;
+
+        //Username settings
+        options.User.AllowedUserNameCharacters =
+            builder
+                .Configuration.GetSection("IdentityConfiguration:AllowedUserNameCharacters")
+                .Value
+            ?? throw new InvalidOperationException("AllowedUserNameCharacters not configured");
+    })
+    .AddEntityFrameworkStores<DataContext>()
+    .AddDefaultTokenProviders();
+#endregion
+
 #region Database Configuration
 Log.Information("Configuring SQLite database...");
 builder.Services.AddDbContext<DataContext>(options =>
     options.UseSqlite(builder.Configuration.GetSection("ConnectionStrings:SqliteDatabase").Value)
 );
+#endregion
+
+var app = builder.Build();
+
+#region Database Migrations
+Log.Information("Migrating and seeding database...");
+using (var scope = app.Services.CreateScope())
+{
+    await DataSeeder.Initialize(app.Services);
+}
 #endregion
 
 app.MapOpenApi();
