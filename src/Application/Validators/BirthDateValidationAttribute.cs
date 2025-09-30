a5bec1b73cfd831de5c3d04278c1ebf847f57503
Rivera-CuentaOficial <146663110+Rivera-CuentaOficial@users.noreply.github.com>
2025-09-30 14:19:01 -0300

    feat(RegisterDTO): Add DTO for the registration functionality alongside custom validators for RUT and BIRTHDATE

---
+++ src/Application/Validators/BirthDateValidationAttribute.cs
@@ -0,0 +1,36 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace TiendaUCN.Application.Validators;
+
+public class BirthDateValidationAttribute : ValidationAttribute
+{
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        string valueString = value?.ToString()!;
+
+        if (!string.IsNullOrEmpty(valueString))
+        {
+            if (!DateTime.TryParse(valueString, out DateTime date))
+            {
+                return new ValidationResult("El formato de la Fecha de Nacimiento no es valido.");
+            }
+            if (date > DateTime.Today)
+            {
+                return new ValidationResult("La fecha de nacimiento no puede ser del futuro.");
+            }
+            if (date < DateTime.Today.AddYears(-120))
+            {
+                return new ValidationResult(
+                    "La fecha de nacimiento no puede ser mayor a 120 años."
+                );
+            }
+            if (date > DateTime.Today.AddYears(-18))
+            {
+                return new ValidationResult(
+                    "La fecha de nacimiento debe ser de una persona mayor de edad."
+                );
+            }
+        }
+        return ValidationResult.Success;
+    }
+}
---
+++ src/Domain/Models/VerificationCode.cs
@@ -0,0 +1,19 @@
+namespace TiendaUCN.Domain.Models;
+
+public enum CodeType
+{
+    EmailVerification,
+    PasswordReset,
+    PasswordChange,
+}
+
+public class VerificationCode
+{
+    public int Id { get; set; }
+    public required CodeType Type { get; set; }
+    public required string Code { get; set; }
+    public int AttemptCount { get; set; } = 0;
+    public required DateTime Expiration { get; set; }
+    public required int UserId { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
