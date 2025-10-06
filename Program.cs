5f5c3765aefeb4037917a4f4108630e6dc6756d9
Bayron <bayron.cruz@alumnos.ucn.cl>
2025-10-05 23:42:11 -0300

    <refactor>[Cambio namespaces y  Actualzicaion de Cloudinary]: Se reorganiuzo los namespaces de todas las clases, para mayor comodida y se implemento cloudinary.

--- Program.cs
+++ Program.cs
@@ -1,22 +1,30 @@
+using Hangfire;
+using Hangfire.Storage.SQLite;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using Resend;
 using Serilog;
-using TiendaUCN.API.Middlewares;
-using TiendaUCN.Application.Infrastructure.Repositories.Implements;
-using TiendaUCN.Application.Infrastructure.Repositories.Interfaces;
-using TiendaUCN.Application.Mappers;
-using TiendaUCN.Application.Services.Implements;
-using TiendaUCN.Application.Services.Interfaces;
-using TiendaUCN.Domain.Models;
-using TiendaUCN.Infrastructure.Data;
-using TiendaUCN.Infrastructure.Repositories.Interfaces;
+using TiendaUCN.src.API.Middlewares;
+using TiendaUCN.src.Application.Jobs.Implements;
+using TiendaUCN.src.Application.Jobs.Interfaces;
+using TiendaUCN.src.Application.Mappers;
+using TiendaUCN.src.Application.Services.Implements;
+using TiendaUCN.src.Application.Services.Interfaces;
+using TiendaUCN.src.Domain.Models;
+using TiendaUCN.src.Infrastructure.Data;
+using TiendaUCN.src.Infrastructure.Repositories.Implements;
+using TiendaUCN.src.Infrastructure.Repositories.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var connectionString = builder.Configuration.GetConnectionString("SqliteDatabase") ?? throw new InvalidOperationException("Connection string SqliteDatabase no configurado");
+
+
 builder.Services.AddScoped<UserMapper>();
 builder.Services.AddScoped<ProductMapper>();
+builder.Services.AddScoped<CartMapper>();
 
 builder.Services.AddOpenApi();
 builder.Services.AddControllers();
@@ -28,6 +36,9 @@ builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped<IVerificationCodeRepository, VerificationCodeRepository>();
 builder.Services.AddScoped<IEmailService, EmailService>();
+builder.Services.AddScoped<IUserJob, UserJob>();
+builder.Services.AddScoped<IFileRepository, FileRepository>();
+builder.Services.AddScoped<IFileService, FileService>();
 
 #region Email Service Configuration
 builder.Services.AddOptions();
@@ -110,19 +121,55 @@ builder.Services.AddDbContext<DataContext>(options =>
 );
 #endregion
 
-var app = builder.Build();
+#region Hangfire Configuration
+Log.Information("Configurando los trabajos en segundo plano de Hangfire");
+var cronExpression = builder.Configuration["Jobs:CronJobDeleteUnconfirmedUsers"] ?? throw new InvalidOperationException("La expresión cron para eliminar usuarios no confirmados no está configurada.");
+var timeZone = TimeZoneInfo.FindSystemTimeZoneById(builder.Configuration["Jobs:TimeZone"] ?? throw new InvalidOperationException("La zona horaria para los trabajos no está configurada."));
+builder.Services.AddHangfire(configuration =>
+{
+    var connectionStringBuilder = new SqliteConnectionStringBuilder(connectionString);
+    var databasePath = connectionStringBuilder.DataSource;
 
-#region Database Migrations
+    configuration.UseSQLiteStorage(databasePath);
+    configuration.SetDataCompatibilityLevel(CompatibilityLevel.Version_170);
+    configuration.UseSimpleAssemblyNameTypeSerializer();
+    configuration.UseRecommendedSerializerSettings();
+});
+builder.Services.AddHangfireServer();
+
+
+#endregion
+var app = builder.Build();
+app.UseHangfireDashboard(builder.Configuration["HangfireDashboard:DashboardPath"] ?? throw new InvalidOperationException("La ruta de hangfire no ha sido declarada"), new DashboardOptions
+{
+    StatsPollingInterval = builder.Configuration.GetValue<int?>("HangfireDashboard:StatsPollingInterval") ?? throw new InvalidOperationException("El intervalo de actualización de estadísticas del panel de control de Hangfire no está configurado."),
+    DashboardTitle = builder.Configuration["HangfireDashboard:DashboardTitle"] ?? throw new InvalidOperationException("El título del panel de control de Hangfire no está configurado."),
+    DisplayStorageConnectionString = builder.Configuration.GetValue<bool?>("HangfireDashboard:DisplayStorageConnectionString") ?? throw new InvalidOperationException("La configuración 'HangfireDashboard:DisplayStorageConnectionString' no está definida."),
+});
+
+#region Database Migrations and jobs Configuration
 Log.Information("Migrating and seeding database...");
 using (var scope = app.Services.CreateScope())
 {
     await DataSeeder.Initialize(app.Services);
+    var jobId = nameof(UserJob.DeleteUnconfirmedAsync);
+    RecurringJob.AddOrUpdate<UserJob>(
+        jobId,
+        job => job.DeleteUnconfirmedAsync(),
+        cronExpression,
+        new RecurringJobOptions
+        {
+            TimeZone = timeZone
+        }
+    );
+    Log.Information($"Job recurrente '{jobId}' configurado con cron: {cronExpression} en zona horaria: {timeZone.Id}");
 
     MapperExtensions.ConfigureMapster(scope.ServiceProvider);
 }
 #endregion
 
 app.UseMiddleware<ExceptionHandlingMiddleware>();
+
 
 app.MapOpenApi();
 app.MapControllers();
